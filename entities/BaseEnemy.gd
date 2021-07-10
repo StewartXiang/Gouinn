3b3a045d7c691b5d57d60d8f50cf2b388dd9d549
liggest <liggest@sina.cn>
2021-07-10 16:06:26 +0800

    enemy 雏形
    能向前走

---
+++ entities/BaseEnemy.gd
@@ -0,0 +1,18 @@
+extends Area2D
+class_name BaseEnemy
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _physics_process(delta):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ entities/DashEnemy.gd
@@ -0,0 +1,23 @@
+extends BaseEnemy
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var speed=20
+var direction=Vector2(0,0)
+var speed_range=Vector2(64,320)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	speed=rand_range(speed_range.x,speed_range.y)
+	direction=Vector2(randf(),randf()).normalized()
+	look_at(direction)
+	direction*=speed
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _physics_process(delta):
+	position+=direction*delta
