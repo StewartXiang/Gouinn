5506aed2c3450d1e9541d11ff3e417fa37da14e3
duolanda <duolanda@live.cn>
2021-07-11 03:26:26 +0800

    沿铁轨走，最高分记录

--- RealMain.gd
+++ RealMain.gd
@@ -14,9 +14,24 @@ func new_game():
 
 func game_over():
 	$ScoreTimer.stop()
+	save_best_score()
+	
 
 func _on_ScoreTimer_timeout():
 	score += 1
 	$HUD.update_score(score)
 
+
+func save_best_score():
+	var best_score = 0
+	var file = File.new()
+	
+	file.open("user://save_game.dat", File.READ)
+	best_score = int(file.get_as_text())
+	file.close()
+	
+	if score > best_score:
+		file.open("user://save_game.dat", File.WRITE)
+		file.store_string(str(score))
+		file.close()
 
--- ui_widgets/MainMenu.gd
+++ ui_widgets/MainMenu.gd
@@ -10,6 +10,10 @@ func on_pressed(name):
 		"NewButton":
 			get_tree().change_scene("res://RealMain.tscn")
 		"ScoreButton":
-			print("分数榜")
+			var file = File.new()
+			file.open("user://save_game.dat", File.READ)
+			var best_score = int(file.get_as_text())
+			file.close()
+			print(best_score)
 		"QuitButton":
 			get_tree().quit()
