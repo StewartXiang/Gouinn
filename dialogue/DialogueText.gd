6380efc47d37bb0ea2e1687df70eed04796ced74
liggest <liggest@sina.cn>
2021-07-11 13:08:32 +0800

    修改的对话

--- dialogue/DialogueText.gd
+++ dialogue/DialogueText.gd
@@ -10,14 +10,15 @@ var show_time:=SHOWTIME
 var on_show:=false
 var current_time:=0.0
 
-onready var text_label:=$BG/Margin/Frame/Center/Text
+onready var text_label:=$BG/Margin/Frame/Margin/Text
 onready var tween:=$Tween
 onready var ani:=$BG/AnimationPlayer
 
 signal on_show_end
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
+func _ready():
+	visible=false
 #	yield(get_tree().create_timer(3.5),"timeout")
 #	dialog_pop_up()
 #	change_text("啊啊啊啊")
@@ -36,17 +37,19 @@ func _process(delta):
 			on_show=false
 			current_time=0.0
 			self.emit_signal("on_show_end")
-#		text_label.percent_visible=p
+		text_label.percent_visible=p
 		
 
 func change_text(s: String,t:float=SHOWTIME):
 	text_label.text=s
-#	text_label.percent_visible=0.0
+	text_label.percent_visible=0.0
 	show_time=t
 	current_time=0.0
 	on_show=true
 	
 func dialog_pop_up():
+	if not visible:
+		visible=true
 	ani.play("pop_up")
 
 func dialog_pop_off():
