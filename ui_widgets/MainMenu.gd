ec4013745fdfcbdd202d9099db7fdaecf1cd7570
duolanda <duolanda@live.cn>
2021-07-11 14:34:36 +0800

    开始菜单

--- ui_widgets/MainMenu.gd
+++ ui_widgets/MainMenu.gd
@@ -1,37 +1,23 @@
 extends Control
 
+var Main = preload("res://RealMain.tscn")
 
-func _ready():
-	for btn in $VBoxContainer.get_children():
-		btn.connect("pressed", self, "on_pressed", [btn.name])
-		
-func on_pressed(name):
-	match name:
-		"NewButton":
-			get_tree().change_scene("res://RealMain.tscn")
-			
-		"ScoreButton":
-			var file = File.new()
-			file.open("user://save_game.dat", File.READ)
-			var best_score = int(file.get_as_text())
-			file.close()
-			
-			$Panel/ScoreLabel.text = "High Score: " + str(best_score)
-			$AnimationPlayer.play("Panel")
-			
-		"QuitButton":
-			get_tree().quit()
+func _on_NewButton_pressed():
+	
+	get_tree().change_scene_to(Main)
 
 
-func _on_ClosedButton_pressed():
-	$AnimationPlayer.play_backwards("Panel")
+func _on_StoryButton_pressed():
+	if $guide_panel.modulate.a == 1:
+#		$AnimationPlayer.play_backwards("GuidePanel")
+		$guide_panel.modulate.a = 0
+	$AnimationPlayer.play("StoryPanel")
+	
 
 
-func _on_FullScreenButton_pressed():
-	OS.set_window_fullscreen(true)
-
-
-func _on_Windows720Button_pressed():
-	OS.set_window_fullscreen(false)
-	OS.set_window_size(Vector2(1280, 720))
-	OS.center_window()
+func _on_GuideButton_pressed():
+	if $story_panel.modulate.a == 1:
+#		$AnimationPlayer.play_backwards("StoryPanel")
+		$story_panel.modulate.a = 0
+	$AnimationPlayer.play("GuidePanel")
+	
